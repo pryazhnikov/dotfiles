fc79c41b22249d62621cb01e8d78891f56158dbc
Victor Pryazhnikov <victor@pryazhnikov.com>
2016-07-01 16:40:27 +0300

    [master]: No line numbers at git grep + git head & git patch commands

--- git/configure.sh
+++ git/configure.sh
@@ -8,13 +8,15 @@ $GIT_SET user.email "victor@pryazhnikov.com"
 $GIT_SET core.editor vi
 $GIT_SET apply.whitespaces nowarn
 $GIT_SET color.ui true
-$GIT_SET grep.lineNumber true
+$GIT_SET grep.lineNumber false
 
 $GIT_SET alias.co checkout
 $GIT_SET alias.ci commit
 $GIT_SET alias.df "diff HEAD"
 $GIT_SET alias.st status
 $GIT_SET alias.br branch
+$GIT_SET alias.head "checkout HEAD"
 $GIT_SET alias.unstash "stash pop"
 $GIT_SET alias.pub "push origin HEAD"
+$GIT_SET alias.patch "diff --patch HEAD"
 $GIT_SET alias.recent "for-each-ref --sort=-committerdate refs/heads/ --format='%(refname:short) %(committer)' --count 10"
